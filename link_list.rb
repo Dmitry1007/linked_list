4a063f28070ffba8f1dd7f34cff6d696911246ae
Dmitry Vizersky <Dmitry@Dmitrys-MacBook-Pro.local>
2015-02-24 13:16:11 -0700

    add Link List

---
+++ link_list.rb
@@ -0,0 +1,111 @@
+class List #IterativeLinkedList
+  # attr_reader :head
+
+  def head
+  	@head  
+  end
+
+  def count
+    if head.nil?
+      0
+    else
+      count = 1
+      current = head
+      while current.link?
+        count += 1
+        current = current.link
+      end
+      count
+    end
+  end
+
+  def push(data)
+    if head.nil?
+      @head = Node.new(data)
+    else
+      last_node.link = Node.new(data)
+    end
+  end
+
+  def last_node
+    current = head
+    while current.link?
+      current = current.link
+    end
+    current
+  end
+
+  def pop
+    if head
+      current = head
+      previous = nil
+      while current.link?
+        previous = current
+        current = current.link
+      end
+
+      if previous.nil?
+        @head = nil
+      else
+        previous.link = nil
+      end
+
+      current.data
+    end
+  end
+
+  def delete(target)
+    if head.data == target
+      @head = head.link
+    else
+      current = head
+      last = nil
+      while current.link?
+        last = current
+        current = current.link
+
+        if current.data == target
+          last.link = current.link
+        end
+      end
+    end 
+  end
+
+  def to_a
+    result = []
+    current = head
+    while current
+      result << current.data
+      current = current.link
+    end
+    return result
+  end
+end
+
+class Node
+  attr_accessor :link
+  attr_reader :data
+
+  def initialize(input_data)
+    @data = input_data
+  end
+
+  def link?
+    link
+  end
+end
+
+if __FILE__ == $0
+list = List.new
+list.push("Hello")
+# list.push("World")
+
+puts list.head
+puts list.last_node
+
+node = Node.new("oh_snap")
+puts node.data
+
+puts list
+
+end
